487a51b4060179c45962f7b8e391b68009cb58e4
Krushnal Bhatti <krushnalbhatti71@gmail.com>
2026-01-10 13:13:04 +0530

    Add files via upload

--- Firebase/confing.js
+++ Firebase/confing.js
@@ -1,21 +1,23 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain:import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MASSAGEINGSENDERID,
-  appId: import.meta.env.VITE_FIREBASE_APPID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
+  // apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
+  // authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
+  // projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  // storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+  // messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: import.meta.env.VITE_FIREBASE_APPID,
+
+    apiKey: "AIzaSyDZJ6bl0d5GbvVz9cDyVtl1SjrXQxrbJ6I",
+  authDomain: "fir-auth-c449b.firebaseapp.com",
+  projectId: "fir-auth-c449b",
+  storageBucket: "fir-auth-c449b.firebasestorage.app",
+  messagingSenderId: "619329501461",
+  appId: "1:619329501461:web:c839a279e872a851a0111c",
+  measurementId: "G-C42F53RLP3"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
